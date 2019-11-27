dfd3ffd7c689685d166c5030ff01e7429f73cac6
ocskier <ocskier@gmail.com>
2019-11-27 01:08:16 -0500

    added progress-ring,20sec timer,questions loop,display correct pic

--- assets/app.js
+++ assets/app.js
@@ -5,12 +5,31 @@ const submitBtn = document.getElementById("submit");
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 
 let inputEls = document.querySelectorAll('input');
+let i = 0;
+let interval = 0;
+let count = 20;
+let progress = 100;
 let userGuess;
+
+window.customElements.define('progress-ring', ProgressRing);
 
 questionArr = [];
 pics = [];
 
-async function start() {
+let newTimeIndicator = document.createElement("div");
+newTimeIndicator.setAttribute("id", "time-indicator");
+newTimeIndicator.setAttribute("style","position: relative;display: flex;justify-content: center;flex-direction: column");
+let progressRing = document.createElement("progress-ring");
+progressRing.setAttribute("stroke",4);
+progressRing.setAttribute("radius",45);
+progressRing.setAttribute("progress",100);
+let spanEl = document.createElement("span");
+spanEl.setAttribute("style","bottom: 55px;position: relative;left: 35px;color: antiquewhite;");
+newTimeIndicator.appendChild(progressRing);
+newTimeIndicator.appendChild(spanEl);
+picEl.append(newTimeIndicator);
+
+async function load() {
   if (localStorage.getItem("questionData") && localStorage.getItem("picData")) {
     questionArr = JSON.parse(localStorage.getItem("questionData"));
     pics = JSON.parse(localStorage.getItem("picData"));
@@ -24,61 +43,23 @@ async function start() {
     } catch (error) {
       console.error(error);
     }
-    try {
-        let googleUrl = 'https://www.googleapis.com//customsearch/v1?key=';
-        googleUrl += process.env.GOOGLE_SEARCH_KEY;
+    for (let i = 0; i < questionArr.length; i++) {
+      try {
+        let googleUrl = 'https://www.googleapis.com//customsearch/v1?';
+        googleUrl += 'key=AIzaSyD_tgpw_aI3elBJ3FQzH5kqi00Qep6jXxM';
         googleUrl += '&cx=016797827939605093875:zpsbjfzch8y';
         googleUrl += '&searchType=image&q=';
-        googleUrl += questionArr[0].correct_answer;
+        googleUrl += questionArr[i].correct_answer;
         googleUrl += '+';
-        googleUrl += questionArr[0].category;
+        googleUrl += questionArr[i].category;
         const picsResponse  = await getData(googleUrl);
-        pics = picsResponse.items;
-        localStorage.setItem("picData", JSON.stringify(pics));
-    } catch (error) {
-        console.log(error);
+        pics.push(picsResponse.items[0].link);
+      } catch (error) {
+          console.log(error);
+      } 
     }
+    localStorage.setItem("picData", JSON.stringify(pics));
   }
-  picEl.setAttribute(
-    "style",
-    `background: url(${pics[0].link});background-size: cover;`
-  );
-  questionEl.textContent = questionArr[0].question;
-  const choices = questionArr[0].incorrect_answers;
-  choices.push(questionArr[0].correct_answer);
-  for (let i = 0; i < choices.length; i++) {
-      let checkboxDiv = document.createElement('div');
-      checkboxDiv.setAttribute('class','mdc-checkbox');
-      let checkboxInput = document.createElement('input');
-      checkboxInput.setAttribute('type',"checkbox")
-      checkboxInput.setAttribute('class', "mdc-checkbox__native-control")
-      checkboxInput.setAttribute("id","checkbox-"+i);
-      checkboxInput.addEventListener("click", uncheckOtherInputs);
-      let checkbox = document.createElement('div');
-      checkbox.setAttribute("class", "mdc-checkbox__background");
-      let checkboxSvg = document.createElement('svg');
-      checkboxSvg.setAttribute("class", "mdc-checkbox__checkmark");
-      checkboxSvg.setAttribute("viewBox", "0 0 24 24");
-      let checkboxPath = document.createElement("path");
-      checkboxPath.setAttribute("class", "mdc-checkbox__checkmark-path")
-      checkboxPath.setAttribute("fill","none")
-      checkboxPath.setAttribute("d","M1.73,12.91 8.1,19.28 22.79,4.59");
-      checkboxSvg.append(checkboxPath);
-      let mixedmarkDiv = document.createElement('div');
-      mixedmarkDiv.setAttribute("class","mdc-checkbox__mixedmark");
-      checkbox.append(checkboxSvg,mixedmarkDiv);
-      let checkboxRipple = document.createElement('div');
-      checkboxRipple.setAttribute("class","mdc-checkbox__ripple");
-      checkboxDiv.append(checkboxInput,checkbox,checkboxRipple);
-      let checkboxLabel = document.createElement('label');
-      checkboxLabel.setAttribute("for","checkbox-"+i);
-      checkboxLabel.textContent = choices[i];
-      let checkboxForm = document.createElement('div');
-      checkboxForm.setAttribute("class","mdc-form-field");
-      checkboxForm.append(checkboxDiv,checkboxLabel);
-      choicesEl.append(checkboxForm);
-  }
-  inputEls = document.querySelectorAll('input');
 }
 
 async function getData(url = "", cors = 'cors') {
@@ -93,6 +74,54 @@ async function getData(url = "", cors = 'cors') {
   return await response.json();
 }
 
+function start () {
+  if (i < questionArr.length) {
+    picEl.setAttribute(
+      "style",
+      `background: url(${pics[i]});background-size: cover;`
+    );
+    choicesEl.innerHTML = "";
+    questionEl.textContent = questionArr[i].question;
+    const choices = questionArr[i].incorrect_answers;
+    choices.push(questionArr[i].correct_answer);
+    for (let i = 0; i < choices.length; i++) {
+        let checkboxDiv = document.createElement('div');
+        checkboxDiv.setAttribute('class','mdc-checkbox');
+        let checkboxInput = document.createElement('input');
+        checkboxInput.setAttribute('type',"checkbox")
+        checkboxInput.setAttribute('class', "mdc-checkbox__native-control")
+        checkboxInput.setAttribute("id","checkbox-"+i);
+        checkboxInput.addEventListener("click", uncheckOtherInputs);
+        let checkbox = document.createElement('div');
+        checkbox.setAttribute("class", "mdc-checkbox__background");
+        let checkboxSvg = document.createElement('svg');
+        checkboxSvg.setAttribute("class", "mdc-checkbox__checkmark");
+        checkboxSvg.setAttribute("viewBox", "0 0 24 24");
+        let checkboxPath = document.createElement("path");
+        checkboxPath.setAttribute("class", "mdc-checkbox__checkmark-path")
+        checkboxPath.setAttribute("fill","none")
+        checkboxPath.setAttribute("d","M1.73,12.91 8.1,19.28 22.79,4.59");
+        checkboxSvg.append(checkboxPath);
+        let mixedmarkDiv = document.createElement('div');
+        mixedmarkDiv.setAttribute("class","mdc-checkbox__mixedmark");
+        checkbox.append(checkboxSvg,mixedmarkDiv);
+        let checkboxRipple = document.createElement('div');
+        checkboxRipple.setAttribute("class","mdc-checkbox__ripple");
+        checkboxDiv.append(checkboxInput,checkbox,checkboxRipple);
+        let checkboxLabel = document.createElement('label');
+        checkboxLabel.setAttribute("for","checkbox-"+i);
+        checkboxLabel.textContent = choices[i];
+        let checkboxForm = document.createElement('div');
+        checkboxForm.setAttribute("class","mdc-form-field");
+        checkboxForm.append(checkboxDiv,checkboxLabel);
+        choicesEl.append(checkboxForm);
+    }
+    inputEls = document.querySelectorAll('input');
+  } else {
+    clearInterval(interval);
+  }
+}
+
 function uncheckOtherInputs(event) {
     const id = parseInt(event.target.id.split('-')[1]);
     inputEls.forEach((el,i)=>{
@@ -104,21 +133,54 @@ function uncheckOtherInputs(event) {
 }
 
 function checkUserGuess() {
+    let answerChecked = false;
     inputEls.forEach((el,i)=>{
         const selectedEl = document.getElementById("checkbox-"+i);
         if(selectedEl.checked === true ) {
             userGuess = i
+            answerChecked = true;
         }
     });
-    if (document.getElementById('checkbox-'+userGuess).parentNode.parentNode.children[1].textContent === questionArr[0].correct_answer) {
+    if (answerChecked){
+      if (document.getElementById('checkbox-'+userGuess).parentNode.parentNode.children[1].textContent === questionArr[i].correct_answer) {
         alert("Correct Answer!")
-    } else {
+      } else {
         alert("Wrong!!!!!!!!!")
+      }
+    } else {
+      alert("No answer checked!");
     }
 }
 
+function decrement() {
+  if(count>0) {
+    count--;
+    newTimeIndicator.children[1].textContent = count;
+    progress -= 5;
+    progressRing.setAttribute('progress',progress);
+  } else {
+    count = 20;
+    progress = 100;
+    i++;
+    start();
+  }
+}
+
+document.addEventListener('scroll', function() {
+    topAppBarElement.setAttribute('class','mdc-top-app-bar mdc-top-app-bar--short mdc-top-app-bar--short-collapsed');
+    topAppBarElement.children[0].children[1].children[0].setAttribute('style','display: none');
+    if(this.scrollingElement.scrollTop===0) {
+        topAppBarElement.setAttribute('class','mdc-top-app-bar mdc-top-app-bar--short');
+        topAppBarElement.children[0].children[1].children[0].setAttribute('style','display: inline-block');
+    }
+});
 submitBtn.addEventListener("click", checkUserGuess);
-start();
+async function run() {
+  await load();
+  start();
+  interval = setInterval(decrement, 1000);
+}
+run();
 
 
 
